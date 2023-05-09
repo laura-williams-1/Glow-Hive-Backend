2a5563ccd23647029e3143d137fcf3d01c416631
laura-williams-1 <laurawilliams@pursuit.org>
2023-05-08 20:49:47 -0400

    minor tweaks to seed, data is good to go :)

--- db/seed.sql
+++ db/seed.sql
@@ -3,16 +3,16 @@
 INSERT INTO products
 
 
-(name, brand, image_url, price, details, size_in_oz, type, vegan, is_cruelty_free, non_toxic, ingredients) 
+(name, brand, image_url, price, details, size_in_oz, type, vegan, is_cruelty_free, non_toxic) 
 
 VALUES
 
 
-('Butta Drop Whipped Oil Body Cream with Tropical Oils + Shea Butter', 'FENTY', 'https://cdn.shopify.com/s/files/1/0341/3458/9485/products/FS393320FSButtaDropMini_1_1200x.jpg?v=1676403236', 44, 'A luxurious, whipped body cream enriched with tropical oils and shea butter for deeply nourished and moisturized skin',6.5, 'moisturizer', true, true,true ),
+('Butta Drop Whipped Oil Body Cream with Tropical Oils + Shea Butter', 'Fenty Skin', 'https://cdn.shopify.com/s/files/1/0341/3458/9485/products/FS393320FSButtaDropMini_1_1200x.jpg?v=1676403236', 44, 'A luxurious, whipped body cream enriched with tropical oils and shea butter for deeply nourished and moisturized skin',6.5, 'moisturizer', true, true,true ),
 
-('Drunk Elephant Protini Polypeptide Cream', 'Drunk Elephant', 'https://example.com/image.jpg', 68, 'A peptide-packed moisturizer with growth factors, supportive amino acids, and pygmy waterlily for immediate improvement in the appearance of skin’s tone, texture, and firmness', 1.69, 'Moisturizer', true, true, true, '{water, glycerin, peptides, etc.}'),
+('Drunk Elephant Protini Polypeptide Cream', 'Drunk Elephant', 'https://example.com/image.jpg', 68, 'A peptide-packed moisturizer with growth factors, supportive amino acids, and pygmy waterlily for immediate improvement in the appearance of skin’s tone, texture, and firmness', 1.69, 'Moisturizer', true, true, true ),
 
-('Lala Retro™ Whipped Cream','Drunk Elephant','https://media.ulta.com/i/ulta/2604051?w=720&fmt=webp', 62,'Lala Retro™ is a whipped dream-come-true for tired, dry skin. This multifaceted rescue cream, infused with six African oils and a plant ceramide complex, provides replenishing barrier support by reinforcing skin’s acid mantle and defending against the effects of everyday stressors.', 1.69, 'Moisturizer',ture, ture, true),
+('Lala Retro™ Whipped Cream','Drunk Elephant','https://media.ulta.com/i/ulta/2604051?w=720&fmt=webp', 62,'Lala Retro™ is a whipped dream-come-true for tired, dry skin. This multifaceted rescue cream, infused with six African oils and a plant ceramide complex, provides replenishing barrier support by reinforcing skin’s acid mantle and defending against the effects of everyday stressors.', 1.69, 'Moisturizer',true, true, true),
 
 ('Milky Jelly Cleanser', 'Glossier', 'https://glossier-prod.imgix.net//products/glossier-mjc-carousel-01.png?v=1680189072&auto=format&q=40&w=459x',19, 'A nourishing and pH-balanced gel-cream cleanser that plays nicely with all skin types. Conditioning ingredients leave skin soft, smooth and never stripped to start your day.',6, 'cleanser', true, true, true),
 
@@ -21,7 +21,11 @@ VALUES
 
 ('Salicylic Acid Acne + Pore Cleanser','The INKEY List', 'https://i5.walmartimages.com/asr/16d5091b-bf29-4fb4-9d43-ef623a20aa84.36ca8ae3aafa7fa0fb0739a61cb1e3cc.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF',12,'Breakout and oil reducing cleanser', 5, 'cleanser',false, true, false),
 (
-'The Deep Cleanse Gentle Exfoliating Cleanser','TATCHA', 'https://assets.tatcha.com/large/large/product-images/cleansers/DeepCleanse-FS-1-main.jpg',40, 'A deep, gentle exfoliating cleanser of natural luffa fruit and wild rose that lifts dirt, minimizes excess oil, and unclogs and tightens pores, leaving skin soft and hydrated.', 5, 'Exfoliater', false, true, false);
+'The Deep Cleanse Gentle Exfoliating Cleanser','TATCHA', 'https://assets.tatcha.com/large/large/product-images/cleansers/DeepCleanse-FS-1-main.jpg',40, 'A deep, gentle exfoliating cleanser of natural luffa fruit and wild rose that lifts dirt, minimizes excess oil, and unclogs and tightens pores, leaving skin soft and hydrated.', 5, 'Exfoliater', false, true, false),
+
+('COOKIES N CLEAN WHIPPED CLAY PORE DETOX FACE MASK WITH SALICYLIC ACID + CHARCOAL','Fenty Skin', 'https://media.kohlsimg.com/is/image/kohls/5539274?wid=1200&hei=1200&op_sharpen=1',35, 'A non-drying whipped clay mask with charcoal that deeply purifies pores. Clinically tested to instantly improve skin texture and fight shine all day.', 2.5, 'Face Mask', true,true, true);
+
+
 
 
 
