cfd6ffda7365a0bde1d80758703d18fb9583b7d8
laura-williams-1 <laurawilliams@pursuit.org>
2023-05-08 20:21:44 -0400

    info added to seed

--- db/schema.sql
+++ db/schema.sql
@@ -2,14 +2,16 @@ DROP DATABASE IF EXISTS glowhive_dev;
 
 CREATE DATABASE glowhive_dev;
 
-\c glowhive_dev
+\c glowhive_dev;
 
 CREATE TABLE products (
 id SERIAL PRIMARY KEY,
 name TEXT NOT NULL,
+brand TEXT NOT NULL,
+image_url TEXT NOT NULL,
 price INT,
 details TEXT NOT NULL,
-size TEXT,
+size_in_oz DECIMAL(5, 2) NOT NULL,
 type TEXT NOT NULL,
 vegan BOOLEAN,
 is_cruelty_free BOOLEAN,
---
+++ db/seed.sql
@@ -0,0 +1,27 @@
+\c glowhive_dev; 
+
+INSERT INTO products
+
+
+(name, brand, image_url, price, details, size_in_oz, type, vegan, is_cruelty_free, non_toxic, ingredients) 
+
+VALUES
+
+
+('Butta Drop Whipped Oil Body Cream with Tropical Oils + Shea Butter', 'FENTY', 'https://cdn.shopify.com/s/files/1/0341/3458/9485/products/FS393320FSButtaDropMini_1_1200x.jpg?v=1676403236', 44, 'A luxurious, whipped body cream enriched with tropical oils and shea butter for deeply nourished and moisturized skin',6.5, 'moisturizer', true, true,true ),
+
+('Drunk Elephant Protini Polypeptide Cream', 'Drunk Elephant', 'https://example.com/image.jpg', 68, 'A peptide-packed moisturizer with growth factors, supportive amino acids, and pygmy waterlily for immediate improvement in the appearance of skin’s tone, texture, and firmness', 1.69, 'Moisturizer', true, true, true, '{water, glycerin, peptides, etc.}'),
+
+('Lala Retro™ Whipped Cream','Drunk Elephant','https://media.ulta.com/i/ulta/2604051?w=720&fmt=webp', 62,'Lala Retro™ is a whipped dream-come-true for tired, dry skin. This multifaceted rescue cream, infused with six African oils and a plant ceramide complex, provides replenishing barrier support by reinforcing skin’s acid mantle and defending against the effects of everyday stressors.', 1.69, 'Moisturizer',ture, ture, true),
+
+('Milky Jelly Cleanser', 'Glossier', 'https://glossier-prod.imgix.net//products/glossier-mjc-carousel-01.png?v=1680189072&auto=format&q=40&w=459x',19, 'A nourishing and pH-balanced gel-cream cleanser that plays nicely with all skin types. Conditioning ingredients leave skin soft, smooth and never stripped to start your day.',6, 'cleanser', true, true, true),
+
+('Cleanser Concentrate', 'Glossier', 'https://glossier-prod.imgix.net/products/glossier-cleanser-concentrate-carousel-01.png?auto=format&q=40&w=540', 21,'A highly concentrated, purifying cleanser that gently exfoliates, clarifies and re-energizes skin in 60 seconds. An A-Team of powerful plant exfoliators—Grape Extract and Lactic Acid—gently smooth skin and brighten your glow, while amino acid surfactants create an undetectable film that holds on to key ingredients so they can work on the skin instead of just rinsing down the drain.',3.3, 'cleanser', true, true, true),
+
+
+('Salicylic Acid Acne + Pore Cleanser','The INKEY List', 'https://i5.walmartimages.com/asr/16d5091b-bf29-4fb4-9d43-ef623a20aa84.36ca8ae3aafa7fa0fb0739a61cb1e3cc.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF',12,'Breakout and oil reducing cleanser', 5, 'cleanser',false, true, false),
+(
+'The Deep Cleanse Gentle Exfoliating Cleanser','TATCHA', 'https://assets.tatcha.com/large/large/product-images/cleansers/DeepCleanse-FS-1-main.jpg',40, 'A deep, gentle exfoliating cleanser of natural luffa fruit and wild rose that lifts dirt, minimizes excess oil, and unclogs and tightens pores, leaving skin soft and hydrated.', 5, 'Exfoliater', false, true, false);
+
+
+
